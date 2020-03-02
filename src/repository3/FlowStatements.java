04ee3e2cdb69ca99da46128c52374c674dd580ec
Shoshone Arnold <eastcoastshony@gmail.com>
2020-03-01 20:27:55 -0500

    added files
    Signed-off-by: Shoshone Arnold <eastcoastshony@gmail.com>

--- src/repository3/FlowStatements.java
+++ src/repository3/FlowStatements.java
@@ -12,11 +12,46 @@ public class FlowStatements {
         }
     }
 
-    public static void ternary(int plastic) {
-        
+    public static void timeOfWeek(int dayOfWeek) {
+        switch (dayOfWeek) {
+            case 6:
+                System.out.println("Today is Saturday");
+                break;
+            case 7:
+                System.out.println("Today is Sunday");
+                break;
+            default:
+                System.out.println("Looking forward to the weekend");
+        }
     }
 
+
     public static void main(String[] args) {
+        timeOfWeek();
         timeOfDay(14);
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
     }
 }
