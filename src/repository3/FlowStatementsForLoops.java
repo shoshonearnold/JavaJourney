faaadd3cb36203eb899a9383fedf1275a7f9df81
Shoshone Arnold <eastcoastshony@gmail.com>
2020-03-05 21:19:10 -0500

    added loop logic
    Signed-off-by: Shoshone Arnold <eastcoastshony@gmail.com>

--- src/repository3/FlowStatementsForLoops.java
+++ src/repository3/FlowStatementsForLoops.java
@@ -9,7 +9,32 @@ public class FlowStatementsForLoops {
         return -1;
     }
 
+    public static boolean isPrime(int n) {
+        if(n == 1) {
+            return false;
+        }
+        for(int i = 2; i <= n/2 // <-- no number that is greater than n / 2 is going to divide into n
+         ; i++) {
+            if(n % i == 0)
+            return false;
+        }
+        int count = 0;
+        for(int c = 10; c < 50; c++) {
+            if(isPrime(c)) {
+                count++;
+                System.out.println("number " + c + " is a prime number");
+                if(count == 3) {
+                    System.out.println("Exiting loop");
+                    break;
+                }
+            }
+        }
+        return true;
+    }
+
+
     public static void main(String[] args) {
         System.out.println(calculateInterest(10000,2.00));
+        isPrime(5);
     }
 }
