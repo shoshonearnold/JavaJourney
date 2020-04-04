84578552e1e2c022e00d9fcad25a911374ba4dbb
Shoshone Arnold <eastcoastshony@gmail.com>
2020-04-04 13:46:59 -0400

    classes logic added
    Signed-off-by: Shoshone Arnold <eastcoastshony@gmail.com>

--- src/repository4/Classes.java
+++ src/repository4/Classes.java
@@ -5,19 +5,46 @@ public class Classes {
     private int doors;
     private int wheels;
     private String model;
-    private String engine;
     private String color;
+    private String engine;
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setWheels(int wheels) {
+        this.wheels = wheels;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
 
     public void setModel(String model) {
-        String validModel = model.toLowerCase();
-        if(validModel.equals("ford") || validModel.equals("porsche")) {
-            this.model = model;
-        } else {
-            this.model = "unknown";
-        }
+        this.model = model;
     }
 
     public String getModel() {
-        return this.model;
+        return model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public String getEngine() {
+        return engine;
     }
 }
--- src/repository4/Main.java
+++ src/repository4/Main.java
@@ -2,9 +2,16 @@ package repository4;
 
 public class Main {
     public static void main(String[] args) {
-        Classes porsche = new Classes();
         Classes ford = new Classes();
-        ford.setModel("F-150");
-        System.out.println("Model is " + ford.getModel());
+        ford.setDoors(4);
+        ford.setWheels(4);
+        ford.setColor("white");
+        ford.setEngine("Idk some engine ffs lol");
+        ford.setModel("f-150");
+        System.out.println("your ford has " + ford.getDoors() + " doors");
+        System.out.println("your ford has " + ford.getWheels() + " wheels");
+        System.out.println("your ford is " + ford.getColor());
+        System.out.println("your ford has " + ford.getEngine());
+        System.out.println("your ford has a " + ford.getModel() + " under the hood");
     }
 }
