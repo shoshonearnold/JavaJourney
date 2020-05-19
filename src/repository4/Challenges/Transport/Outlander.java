2d1baa4230a137bb9c6667f65f4818f1e6f40238
shoshone.arnold <eastcoastshony@gmail.com>
2020-05-19 11:56:43 -0400

    constructor logic

--- src/repository4/Challenges/Transport/Outlander.java
+++ src/repository4/Challenges/Transport/Outlander.java
@@ -7,4 +7,15 @@ public class Outlander extends Car {
         super("Outlander", "4WD", 5/*+ spare*/, 6/*+ hood,trunk*/, 6, false);
         this.roadServiceMonths = roadServiceMonths;
     }
+
+    public void accelerate(int rate) {
+
+        int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity == 0) {
+            stop();
+        } else if(newVelocity > 0 && newVelocity <= 10) {
+            changeGear(1);
+        }
+
+    }
 }
